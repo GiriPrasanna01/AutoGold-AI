279b57a72e6d1aaa84b26b0e647ec42263d63b2c
GiriPrasanna01 <giriprasanna.it.2023@snsct.org>
2025-06-10 17:09:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-4xl font-bold">Welcome to AutoGold.AI</h1>
+        <p>Get your daily boss quotes, memes, and more!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
